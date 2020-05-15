17ae25e94435e0bcddc20012efcffec8bb86af10
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-05-15 13:56:22 -0700

    playing with the this keyword

--- 01-EarlySections/2-19.5-JS-this/app.js
+++ 01-EarlySections/2-19.5-JS-this/app.js
@@ -10,11 +10,25 @@ class NameField {
 class NameGenerator {
     constructor() {
         const btn = document.querySelector('button');
-        btn.addEventListener('click', ???);
+        this.names = ['Max', 'Manu', 'Anna'];
+        this.currentName = 0;
+        // btn.addEventListener('click', ???);
+        //btn.addEventListener('click', this.addName.bind(this));
+        // btn.addEventListener('click', function () {
+        //     this.addName(); // doesn't work
+        // });
+        btn.addEventListener('click', () => {
+            this.addName(); // doesn't work
+        });
     }
 
     addName() {
-        const name = new NameField("Max");
+        console.log(this);
+        const name = new NameField(this.names[this.currentName]);
+        this.currentName++;
+        if (this.currentName >= this.names.length) {
+            this.currentName = 0;
+        }
     }
 }
 const gen = new NameGenerator();
