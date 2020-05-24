ddd0625ef0c4ac3642322a11cf23df71f84c2304
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-05-24 14:08:17 -0700

    Node work for today.

--- 03-Basics/03-26-hello/app.js
+++ 03-Basics/03-26-hello/app.js
@@ -2,7 +2,17 @@ const http = require('http');
 
 
 const server = http.createServer((req, res)=>{
-    console.log(req);
+    console.log('req.url: ' + req.url);
+    console.log('req.method: ' + req.method);
+    console.log('req.headers: ' + req.headers);
+    console.log(req.url, req.method, req.headers);
+    
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
