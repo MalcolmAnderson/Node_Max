c4ced2a364885603c61f6788765d10eee02f5bf8
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-06-12 07:51:15 -0700

    complete 04-43

---
+++ 04-Dev-Work-Debug/04-43-npm/routes.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My Page</title></head>');
+        res.write('<body><h1>Assignment 1 - Some greeting in root!</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/users') {
+        res.write('<html>');
+        res.write('<ul>');
+        res.write('<li>user1</li>');
+        res.write('<li>user2</li>');
+        res.write('</ul>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/cu' || url === '/create-user-page') {
+        res.write('<html>');
+        res.write('<head><title>Add User</title></head>');
+        res.write('<body><form action="/create-user" method="POST"><input type="text" name="message"><button type="submit">Add User</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/create-user' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody.split("=")[1]);
+        });
+        res.statusCode = 302; 
+        res.setHeader('Location', '/');
+        res.end();
+    }
+};
+
+exports.handler = requestHandler;
+exports.someText = 'Some hard coded text';
