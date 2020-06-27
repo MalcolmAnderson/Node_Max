1a520453eaba3504637f444ab4494686135816a2
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-06-26 22:16:16 -0700

    Save assignment changes

--- 05-Express/05-75-Assignment/app.js
+++ 05-Express/05-75-Assignment/app.js
@@ -2,17 +2,12 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mainRoutes = require('./routes/index');
 
 const app = express();
 
-const routes = require('./routes/routes');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/');
-
-app.use((req, res, next)=>{
-    res.status(404).send('<h1>404</h1>');
-})
+app.use(mainRoutes);
 
 app.listen(3000);
